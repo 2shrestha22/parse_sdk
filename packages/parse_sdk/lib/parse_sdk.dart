a4c2bf3bbc15a96316d76a3d7121c6f584e1fd31
Sangam Shrestha <2shrestha22@gmail.com>
2022-08-10 20:10:27 +0545

    chore: project updates

--- packages/parse_sdk/lib/parse_sdk.dart
+++ packages/parse_sdk/lib/parse_sdk.dart
@@ -1,5 +1,11 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
 library parse_sdk;
 
 export 'src/parse_client.dart';
 export 'src/services/database.dart';
 export 'src/services/parse_storage.dart';
+
+
+// TODO: Export any libraries intended for clients of this package.
